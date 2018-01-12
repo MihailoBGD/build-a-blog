f60dcf2257b74bd7542529ec264b7056e9efc7e5
MihailoBGD <mnsbgd@yahoo.com>
2018-01-12 11:18:48 -0500

    touched up code

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://get-it-done:beproductive@localhost:8889/get-it-done'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:lozinka@localhost:8889/build-a-blog'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 
@@ -30,7 +30,7 @@ def index():
 
     tasks = Task.query.filter_by(completed=False).all()
     completed_tasks = Task.query.filter_by(completed=True).all()
-    return render_template('todos.html',title="Get It Done!", 
+    return render_template('todos.html',title="Build-a-blog", 
         tasks=tasks, completed_tasks=completed_tasks)
 
 
@@ -47,4 +47,4 @@ def delete_task():
 
 
 if __name__ == '__main__':
-app.run()
+    app.run()
